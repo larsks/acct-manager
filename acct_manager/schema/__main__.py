7a461e9adfd598490746e6ed15b0f4ddf91afa0a
Lars Kellogg-Stedman <lars@redhat.com>
2021-12-18 16:03:01 -0500

    Move schema helper functions into .schema.helpers

--- acct_manager/schema/__main__.py
+++ acct_manager/schema/__main__.py
@@ -1,26 +1,16 @@
 """Generate jsonschema descriptions of API objects"""
 
-from typing import Any
-from pydantic.main import ModelMetaclass
-from pydantic.schema import schema
 import yaml
 
 # pylint: disable=import-error
-from acct_manager import models
+from acct_manager.schema import helpers
 
 
-def generate_schema() -> dict[str, Any]:
-    """Return schema dictionary for API models"""
-
-    selected = []
-    for modelname in dir(models):
-        model = getattr(models, modelname)
-        if isinstance(model, ModelMetaclass) and getattr(model, "_expose", False):
-            selected.append(model)
-
-    return schema(selected, title="Onboarding Microservice API")
+def main() -> None:
+    """Print YAML version of schema"""
+    defs = helpers.generate_schema()
+    print(yaml.safe_dump(defs))
 
 
 if __name__ == "__main__":
-    defs = generate_schema()
-    print(yaml.safe_dump(defs))
+    main()
